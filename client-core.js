f9a92763d3896a323d21017f6618b0cb32b61265
Hazipov Vladislav <hazipov.vlad@yandex.ru>
2017-12-17 02:07:00 +0500

    no checks

--- client-core.js
+++ client-core.js
@@ -1,11 +1,126 @@
 'use strict';
 
 module.exports.execute = execute;
-module.exports.isStar = true;
+module.exports.isStar = false;
+
+const request = require('request');
+const minimist = require('minimist');
+
+const URL = 'http://localhost:8080/messages';
+const chalk = require('chalk');
+const red = chalk.hex('#f00');
+const green = chalk.hex('#0f0');
 
 function execute() {
     // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
+    const args = minimist(process.argv.slice(3));
+    var from = args.from;
+    var to = args.to;
+    var text = args.text;
+    var command = process.argv[2];
+    switch (command) {
+        case 'send':
+            return sendMassege(from, to, text);
+        case 'list':
+            return listMasseges(from, to);
+        default:
+            Promise.reject('Нет такой команды');
+            break;
+    }
+}
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+function listMasseges(from, to) {
+    return new Promise ((resolve, reject) => {
+        var options = {
+            uri: URL,
+            qs: rightData(from, to)
+        };
+        request.get(options)
+
+            .on('response', res => {
+                var body = '';
+
+                res.on('data', chunk => {
+                    body += chunk;
+                });
+
+                res.on('error', () => {
+                    reject('error');
+                });
+
+                res.on('end', () => {
+                    var coolBody = makeBodyGreatAgain(JSON.parse(body));
+                    resolve(coolBody);
+                });
+            });
+    });
+}
+
+function sendMassege(from, to, text) {
+    return new Promise ((resolve, reject) => {
+        var options = {
+            uri: URL,
+            qs: rightData(from, to),
+            form: JSON.stringify({ text })
+        };
+
+        request.post(options)
+
+            .on('response', res => {
+                var body = '';
+
+                res.on('data', chunk => {
+                    body += chunk;
+                });
+
+                res.on('error', () => {
+                    reject('error');
+                });
+
+                res.on('end', () => {
+                    var tempArray = [];
+                    tempArray.push(JSON.parse(body));
+                    var coolBody = makeBodyGreatAgain(tempArray);
+                    resolve(coolBody);
+                });
+            });
+    });
+}
+
+function rightData(from, to) {
+    var result = {};
+
+    if (from !== undefined) {
+        result.from = from;
+    }
+
+    if (to !== undefined) {
+        result.to = to;
+    }
+
+    return result;
+}
+
+function makeBodyGreatAgain(body) {
+    var result = [];
+    body.forEach(function (element) {
+
+        var str = '';
+        if (element.from !== undefined) {
+            str += red('FROM') + ': ' + element.from + '\n';
+        }
+
+        if (element.to !== undefined) {
+            str += red('TO') + ': ' + element.to + '\n';
+        }
+
+        if (element.text !== undefined) {
+            str += green('TEXT') + ': ' + element.text;
+        }
+
+        result.push(str);
+    });
+
+
+    return result.join('\n\n');
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,78 @@
 'use strict';
 
 const http = require('http');
-
+const url = require('url');
+const qs = require('querystring');
 const server = http.createServer();
+var messages = [];
 
 server.on('request', (req, res) => {
     // Тут нужно обработать запрос
-    res.end();
+    // const pathname = url.parse(req.url).pathname;
+    const query = url.parse(req.url).query;
+    const fromAndTo = qs.parse(query);
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method === 'GET') {
+        var filtredData = JSON.stringify(getFiltredData(fromAndTo));
+        res.write(filtredData);
+        res.end();
+    }
+
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
+        });
+
+        req.on('end', () =>{
+            var text = JSON.parse(body).text;
+            messages.push(getData(fromAndTo, text));
+            res.write(JSON.stringify(messages[messages.length - 1]));
+            res.end();
+        });
+    }
+
 });
+
+function getFiltredData(fromAndTo) {
+    var result = messages;
+
+    function fltr(element) {
+        if (fromAndTo.from !== undefined) {
+            return element.from === fromAndTo.from;
+        }
+
+        return true;
+    }
+
+    function fltr2(element) {
+        if (fromAndTo.to !== undefined) {
+            return element.to === fromAndTo.to;
+        }
+
+        return true;
+    }
+
+
+    return result.filter(fltr).filter(fltr2);
+}
+
+function getData(fromAndTo, text) {
+    var result = {};
+    if (fromAndTo.from) {
+        result.from = fromAndTo.from;
+    }
+
+    if (fromAndTo.to) {
+        result.to = fromAndTo.to;
+    }
+
+    result.text = text;
+
+    return result;
+}
+
 
 module.exports = server;
