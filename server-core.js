21495c4c30a9d4e57d3e0ff14f340fbb92493e66
Hazipov Vladislav <hazipov.vlad@yandex.ru>
2017-12-18 01:56:27 +0500

    sfd

--- server-core.js
+++ server-core.js
@@ -17,18 +17,8 @@ server.on('request', (req, res) => {
         res.write(filtredData);
         res.end();
     } else if (req.method === 'POST' && urlTest) {
-        var body = '';
-
-        req.on('data', chunk => {
-            body += chunk;
-        });
-
-        req.on('end', () =>{
-            var text = JSON.parse(body).text;
-            messages.push(getData(fromAndTo, text));
-            res.write(JSON.stringify(getData(fromAndTo, text)));
-            res.end();
-        });
+        res.statuscode = 404;
+        res.end();
     } else {
         res.statuscode = 404;
         res.end();
@@ -60,6 +50,7 @@ function getFiltredData(fromAndTo) {
     return result;
 }
 
+/*
 function getData(fromAndTo, text) {
     var result = {};
     if (fromAndTo.from) {
@@ -73,7 +64,7 @@ function getData(fromAndTo, text) {
     result.text = text;
 
     return result;
-}
+}*/
 
 
 module.exports = server;
